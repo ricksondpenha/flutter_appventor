4dc57d4300b8ad534adb6f5b6f6cb47800beb047
Rickson Dpenha <ricksondpenha@gmail.com>
2022-11-15 15:33:13 +0530

    infra: project restructure

--- lib/app.dart
+++ lib/app.dart
@@ -1,41 +1,24 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'flavors.dart';
-import 'pages/my_home_page.dart';
+class RootApp extends StatelessWidget {
+  const RootApp({Key? key}) : super(key: key);
 
-class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: F.title,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: _flavorBanner(
-        child: MyHomePage(),
-        show: kDebugMode,
-      ),
-    );
-  }
-
-  Widget _flavorBanner({
-    required Widget child,
-    bool show = true,
-  }) =>
-      show
-          ? Banner(
-        child: child,
-        location: BannerLocation.topStart,
-        message: F.name,
-        color: Colors.green.withOpacity(0.6),
-        textStyle: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 12.0,
-            letterSpacing: 1.0),
-        textDirection: TextDirection.ltr,
-      )
-          : Container(
-        child: child,
+    return Consumer(builder: (context, ref, child) {
+      return MaterialApp(
+        theme: FlexThemeData.light(scheme: FlexScheme.outerSpace),
+        darkTheme: FlexThemeData.dark(scheme: FlexScheme.outerSpace),
+        themeMode: ThemeMode.system,
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Appventor'),
+          ),
+          body: const FlutterLogo(),
+        ),
       );
+    });
+  }
 }
--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
-import 'flavors.dart';
+import 'config/flavors.dart';
 
 void main() {
   F.appFlavor = Flavor.DEV;
-  runApp(App());
+  runApp(const ProviderScope(child: RootApp()));
 }
