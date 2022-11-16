a31d33c09bda6a5779ec1c4921db80974a0b8e9c
Rickson Dpenha <ricksondpenha@gmail.com>
2022-11-16 14:18:03 +0530

    infra: adds commonly used constants

---
+++ lib/config/constants.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+//padding
+const padding8 = EdgeInsets.all(8);
+const padding16 = EdgeInsets.all(16);
+const padding24 = EdgeInsets.all(24);
+const padding32 = EdgeInsets.all(32);
+const paddingH8 = EdgeInsets.symmetric(horizontal: 8);
+const paddingH16 = EdgeInsets.symmetric(horizontal: 16);
+const paddingV8 = EdgeInsets.symmetric(vertical: 8);
+const paddingV16 = EdgeInsets.symmetric(vertical: 16);
+
+//common widgets
+const emptyWidget = SizedBox.shrink();
+const spaceH8 = SizedBox(width: 8);
+const spaceH16 = SizedBox(width: 16);
+const spaceH24 = SizedBox(width: 24);
+const spaceH32 = SizedBox(width: 32);
+const spaceV8 = SizedBox(height: 8);
+const spaceV16 = SizedBox(height: 16);
+const spaceV24 = SizedBox(height: 24);
+const spaceV32 = SizedBox(height: 32);
