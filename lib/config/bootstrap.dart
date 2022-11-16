77a1cd644dfff359d9fd26d5c086c04e5ec9f621
Rickson Dpenha <ricksondpenha@gmail.com>
2022-11-16 14:04:11 +0530

    infra: bootstrap providers and init firebase configs

---
+++ lib/config/bootstrap.dart
@@ -0,0 +1,66 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_appventor/config/firebase/firebase_options_dev.dart'
+    as dev;
+import 'package:flutter_appventor/config/firebase/firebase_options_beta.dart'
+    as beta;
+import 'package:flutter_appventor/config/firebase/firebase_options_prod.dart'
+    as prod;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'flavors.dart';
+
+Future<ProviderContainer> bootstrap() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  switch (F.appFlavor) {
+    case Flavor.DEV:
+      await Firebase.initializeApp(
+        options: dev.DefaultFirebaseOptions.currentPlatform,
+      );
+      break;
+    case Flavor.BETA:
+      await Firebase.initializeApp(
+        options: beta.DefaultFirebaseOptions.currentPlatform,
+      );
+      break;
+    case Flavor.PROD:
+      await Firebase.initializeApp(
+        options: prod.DefaultFirebaseOptions.currentPlatform,
+      );
+      break;
+    default:
+      await Firebase.initializeApp(
+        options: prod.DefaultFirebaseOptions.currentPlatform,
+      );
+  }
+// beta.DefaultFirebaseOptions
+  final container = ProviderContainer(
+    overrides: [],
+    observers: [if (F.appFlavor == Flavor.DEV) ProviderLogger()],
+  );
+
+  ///#initialize all the required providers here
+  // await container.listen(, (previous, next) { });
+  // await container.read(provider)
+  return container;
+}
+
+class ProviderLogger extends ProviderObserver {
+  @override
+  void didUpdateProvider(
+    ProviderBase<dynamic> provider,
+    Object? previousValue,
+    Object? newValue,
+    ProviderContainer container,
+  ) {
+    debugPrint(
+      '''
+      {
+      "provider": "${provider.name ?? provider.runtimeType}",
+      "newValue": "$newValue"
+      }''',
+    );
+  }
+}
--- lib/main_Beta.dart
+++ lib/main_Beta.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
+import 'config/bootstrap.dart';
 import 'config/flavors.dart';
 
-void main() {
+void main() async {
   F.appFlavor = Flavor.BETA;
-  runApp(const ProviderScope(child: RootApp()));
+  runApp(UncontrolledProviderScope(
+    container: await bootstrap(),
+    child: const RootApp(),
+  ));
 }
