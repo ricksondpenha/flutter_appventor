00d17c0776fb91ccdb513c23896c29cfb08ed45a
Rickson Dpenha <ricksondpenha@gmail.com>
2022-11-23 23:09:30 +0530

    docs: move constants to helpers and document them

--- lib/core/helpers/constants.dart
+++ lib/core/helpers/constants.dart
@@ -1,22 +1,52 @@
 import 'package:flutter/material.dart';
 
-//padding
+/// adds padding on all sides at 8 pixels
 const padding8 = EdgeInsets.all(8);
+
+/// adds padding on all sides at 16 pixels
 const padding16 = EdgeInsets.all(16);
+
+/// adds padding on all sides at 24 pixels
 const padding24 = EdgeInsets.all(24);
+
+/// adds padding on all sides at 32 pixels
 const padding32 = EdgeInsets.all(32);
+
+/// adds padding horizontally at 8 pixels
 const paddingH8 = EdgeInsets.symmetric(horizontal: 8);
+
+/// adds padding horizontally at 16 pixels
 const paddingH16 = EdgeInsets.symmetric(horizontal: 16);
+
+/// adds padding vertically at 8 pixels
 const paddingV8 = EdgeInsets.symmetric(vertical: 8);
+
+/// adds padding vertically at 16 pixels
 const paddingV16 = EdgeInsets.symmetric(vertical: 16);
 
-//common widgets
+/// renders an empty widget
 const emptyWidget = SizedBox.shrink();
+
+/// adds spacing horizontally at 8 pixels
 const spaceH8 = SizedBox(width: 8);
+
+/// adds spacing horizontally at 16 pixels
 const spaceH16 = SizedBox(width: 16);
+
+/// adds spacing horizontally at 24 pixels
 const spaceH24 = SizedBox(width: 24);
+
+/// adds spacing horizontally at 32 pixels
 const spaceH32 = SizedBox(width: 32);
+
+/// adds spacing vertically at 8 pixels
 const spaceV8 = SizedBox(height: 8);
+
+/// adds spacing vertically at 16 pixels
 const spaceV16 = SizedBox(height: 16);
+
+/// adds spacing vertically at 24 pixels
 const spaceV24 = SizedBox(height: 24);
+
+/// adds spacing vertically at 32 pixels
 const spaceV32 = SizedBox(height: 32);
